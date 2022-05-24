50297a6a8561592b072e98c265e62f048f2ab078
kanghorim <horim159456@naver.com>
2022-05-24 15:48:30 +0900

    게임추천 시각화

--- job08_Game_recommendation.py
+++ job08_Game_recommendation.py
@@ -4,6 +4,18 @@ from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread, mmwrite
 import pickle
 from gensim.models import Word2Vec
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import font_manager, rc
+import matplotlib as mpl
+import squarify # pip install squarify
+
+font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(
+    fname=font_path).get_name()
+mpl.rcParams['axes.unicode_minus']=False
+rc('font', family=font_name)
+
 
 df_reviews = pd.read_csv('./crawling_data/datasets/Game_reviews_ALL_Preprocessing_2.csv')
 df_reviews.info()
@@ -12,8 +24,8 @@ df_reviews.info()
 # 1. getRecommendation라는 함수를 선언하고 유사도와 인덱스값을 한번에 보여주는 코드
 def getRecommendation(cosine_sim):
     simScore = list(enumerate(cosine_sim[-1]))
-    print(len(simScore))
-    print(simScore) # 유사도 출력
+    #print(len(simScore))
+    #print("ㅁㄴㅇ",simScore)
     simScore = sorted(simScore, key=lambda x:x[1],
                       reverse=True) # 유사도를 sorted함수로 정리
     simScore = simScore[1:11] # 유사도 높은 순서대로 10개 출력
@@ -43,7 +55,46 @@ for i, word in enumerate(words):
 sentence = ' '.join(sentence)
 sentence_vec = Tfidf.transform([sentence])
 cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+#a.sort(reverse=True)
+singular_cosine_sim = cosine_sim[0]
+y_result = sorted(singular_cosine_sim)
+y_result.sort(reverse=True)
+y_result = y_result[0:10]
+
+round_y = []
+for y in y_result:
+    round_y.append(round(y,2) * 100)
+
+
+print('debug',y_result)
 
 # Word2Vec과 Tfidf_matrix를 이용한 종합 게임 추천
 recommendation = getRecommendation(cosine_sim)
 print(recommendation)
+
+#나온 결과물 시각화하기 - 막대그래프
+# plt.figure(figsize=(8, 8))
+# x = np.arange(10)
+# plt.bar(x, y_result)
+# plt.xticks(x, recommendation,rotation= -90)
+# plt.show()
+
+#나온 결과물 시각화하기 - 원그래프
+# plt.figure(figsize=(8, 8))
+# plt.title("게임추천")
+# data= y_result
+# label= recommendation
+#
+# plt.axis('equal')
+# plt.pie(data, labels=label,autopct='%.1f%%')
+# #plt.legend()
+# plt.show()
+plt.figure(figsize=(12, 12))
+sizes= y_result # 상위 10개 게임의 유사도 리스트
+label= recommendation # 상위 10개 게임 이름 리스트
+color=['red','blue','green','grey','lightgreen', 'cornflowerblue', 'mediumpurple', 'lightcoral','lightgreen', 'cornflowerblue']
+#squarify.plot(sizes=sizes, label=label, color=color, alpha=0.6 )
+squarify.plot(sizes = sizes, label=label, color=color,value = round_y,
+              bar_kwargs=dict(linewidth=5, edgecolor="#eee"))
+plt.axis('off')
+plt.show()
